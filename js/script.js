dee2560e9b3dfc09989bcc79f96fb3cce61faab7
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2020-09-22 11:13:52 +0200

    practices finished
    added bundle.min.js in bottom of index page,
    created script.js
    added list of colors and random variable and method eventListener each and every time i click on button it will change backgroundColor on it

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log('hello ');
+let colors = ['yellow','green','blue','#f15025','rgba(125,125,125,0.5)'];
+
+let btn = document.getElementById('btn');
+//add event listener
+btn.addEventListener('click',function(){
+    let random = Math.floor(Math.random() * colors.length);
+    let body = document.body;
+    body.style.backgroundColor = colors[random];
+    console.log(random);
+})
